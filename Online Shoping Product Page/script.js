8bc7847ecffd3f2faff0dea92bb487a74e70f1d3
kalpesh katariya <115180682+kalpeshkatariya@users.noreply.github.com>
2023-03-07 10:54:44 +0530

    Create script.js

---
+++ Online Shoping Product Page/script.js
@@ -0,0 +1,74 @@
+
+
+
+
+document.querySelector('.img1').addEventListener('click', function () {
+    document.querySelector('.img5 ').style.outline = 'none';
+    document.querySelector('.img2').style.outline = 'none';
+    document.querySelector('.img3').style.outline = 'none';
+    document.querySelector('.img4').style.outline = 'none';
+    document.querySelector('.img1').style.outline = '2px solid rgb(0, 150, 136) ';
+
+    document.getElementById('main-images').src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg';
+
+});
+
+document.querySelector('.img2').addEventListener('click', function () {
+
+    document.querySelector('.img1').style.outline = 'none';
+    document.querySelector('.img2').style.outline = '2px solid rgb(0, 150, 136)';
+    document.querySelector('.img3').style.outline = 'none';
+    document.querySelector('.img4').style.outline = 'none';
+    document.querySelector('.img5').style.outline = 'none';
+
+    document.getElementById('main-images').src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/efc3d5b9-1bb3-4427-af53-7acae7af98951541402833591-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-2.jpg';
+
+
+
+
+});
+
+document.querySelector('.img3').addEventListener('click', function () {
+
+    document.querySelector('.img1').style.outline = 'none';
+    document.querySelector('.img3').style.outline = '2px solid rgb(0, 150, 136)';
+    document.querySelector('.img2').style.outline = 'none';
+    document.querySelector('.img4').style.outline = 'none';
+    document.querySelector('.img5').style.outline = 'none';
+
+    document.getElementById('main-images').src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/c7e58861-3431-4189-9903-9880f5eebd181541402833566-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-3.jpg';
+
+});
+
+document.querySelector('.img4').addEventListener('click', function () {
+
+    document.querySelector('.img1').style.outline = 'none';
+    document.querySelector('.img4').style.outline = '2px solid rgb(0, 150, 136)';
+    document.querySelector('.img3').style.outline = 'none';
+    document.querySelector('.img2').style.outline = 'none';
+    document.querySelector('.img5').style.outline = 'none';
+
+    document.getElementById('main-images').src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/66490b64-32de-44b4-a6e4-fe36f1c040051541402833548-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-4.jpg';
+
+});
+
+document.querySelector('.img5').addEventListener('click', function () {
+
+    document.querySelector('.img1').style.outline = 'none';
+    document.querySelector('.img5').style.outline = '2px solid rgb(0, 150, 136)';
+    document.querySelector('.img3').style.outline = 'none';
+    document.querySelector('.img4').style.outline = 'none';
+    document.querySelector('.img2').style.outline = 'none';
+
+    document.getElementById('main-images').src = 'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/957be784-7c5d-4e90-ab9f-0928015b22891541402833645-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-5.jpg';
+
+});
+
+
+
+
+
+
+
+
+
